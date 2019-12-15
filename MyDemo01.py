afe477353080cdeb68ee9334eeedd4bced47cf73
lvjie1 <lvjie1@xiaomi.com>
2019-12-15 10:23:34 +0800

    python 格式问题

--- MyDemo01.py
+++ MyDemo01.py
@@ -2,4 +2,18 @@
 # Python中默认的编码格式是 ASCII 格式，在没修改编码格式时无法正确打印汉字，所以在读取中文时会报错
 # 解决方法为只要在文件开头加入 # -*- coding: UTF-8 -*- 或者 # coding=utf-8 就行了
 
-print("你好，世界")
+# 2019.11.30日学习
+# print("你好，世界")
+#
+# word = 'word'
+# sentence = "这是一个句子。"
+# paragraph = """这是一个段落。
+#     包含了多个语句"""
+#
+# print(word)
+# print(sentence)
+# print(paragraph)
+
+
+# 学到这里
+# https://www.runoob.com/python/python-variable-types.html
